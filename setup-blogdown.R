480eca942d7e780fb6f8a04854ab14a256f0c9eb
anagelberg <62032317+anagelberg@users.noreply.github.com>
2023-03-02 15:37:33 -0800

    Adding website template

---
+++ setup-blogdown.R
@@ -0,0 +1,11 @@
+#install.packages('blogdown')
+
+
+
+blogdown::new_site(theme='kishaningithub/hugo-creative-portfolio-theme')
+
+blogdown::build_site()
+
+blogdown::serve_site()
+
+blogdown::stop_server()
